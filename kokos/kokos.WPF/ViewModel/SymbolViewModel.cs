ef18bd4015e5b26a419675f826830ad23c1a5ef6
cezary12 <katner@gmail.com>
2014-05-10 17:36:17 +0200

    nuget oxyplot added + candlestick chart

--- kokos/kokos.WPF/ViewModel/SymbolViewModel.cs
+++ kokos/kokos.WPF/ViewModel/SymbolViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using kokos.WPF.ServerConnect;
 using kokos.WPF.ViewModel.Base;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using ReactiveUI;
 using System;
 using System.Collections.ObjectModel;
@@ -40,10 +43,23 @@ namespace kokos.WPF.ViewModel
             set { this.RaiseAndSetIfChanged(ref _isBusy, value); }
         }
 
+        private bool _isLoaded;
+        public bool IsLoaded
+        {
+            get { return _isLoaded; }
+            set { this.RaiseAndSetIfChanged(ref _isLoaded, value); }
+        }
+
+        private PlotModel _plot;
+        public PlotModel Plot
+        {
+            get { return _plot; }
+            set { this.RaiseAndSetIfChanged(ref _plot, value); }
+        }
+
         public ObservableCollection<TickData> Ticks { get; private set; } 
 
         public IReactiveCommand LoadTickData { get; private set; }
-
 
         public SymbolViewModel()
         {
@@ -52,13 +68,14 @@ namespace kokos.WPF.ViewModel
 
             Ticks = new ObservableCollection<TickData>();
             LoadTickData = ReactiveCommand.CreateAsync(this.WhenAny(x => x.IsBusy, x => !x.Value), ExecuteLoadTickDataAsync, RxApp.MainThreadScheduler);
+            Plot = CreateCandleStickSeries();
         }
 
         private async Task<bool> ExecuteLoadTickDataAsync(object parameter)
         {
             IsBusy = true;
 
-            var period = PERIOD_CODE.PERIOD_M5;
+            var period = PERIOD_CODE.PERIOD_D1;
 
             var endDate = DateTime.Now;
 
@@ -73,9 +90,38 @@ namespace kokos.WPF.ViewModel
             foreach (var tick in ticks)
                 Ticks.Add(tick);
 
+            Plot = CreateCandleStickSeries();
             IsBusy = false;
+            IsLoaded = true;
 
             return true;
+        }
+
+        private PlotModel CreateCandleStickSeries()
+        {
+            var pm = new PlotModel { Title = Name, LegendSymbolLength = 24 };
+
+            var timeSpanAxis1 = new DateTimeAxis { Position = AxisPosition.Bottom, StringFormat = "MM/dd/yyyy" };
+            pm.Axes.Add(timeSpanAxis1);
+            var linearAxis1 = new LinearAxis { Position = AxisPosition.Left, StringFormat = "N4"};
+            pm.Axes.Add(linearAxis1);
+            var candleStickSeries = new CandleStickSeries
+            {
+                //CandleWidth = 6,
+                Title = Name,
+                Color = OxyColor.FromRgb(57, 58, 59), //black
+                IncreasingFill = OxyColor.FromRgb(17, 178, 64), //green
+                DecreasingFill = OxyColor.FromRgb(178, 36, 35), //red
+                DataFieldX = "Time",
+                DataFieldHigh = "High",
+                DataFieldLow = "Low",
+                DataFieldOpen = "Open",
+                DataFieldClose = "Close",
+                TrackerFormatString = "{1:MM/dd/yyyy HH:mm:ss}\nOpen: {4:N4}\nHigh: {2:N4}\nLow: {3:N4}\nClose: {5:N4}",
+                ItemsSource = (Ticks ?? new ObservableCollection<TickData>())
+            };
+            pm.Series.Add(candleStickSeries);
+            return pm;
         }
     }
 }
