31883dda960f45baf2fb239cf01fd9390e3a19bb
cezary12 <katner@gmail.com>
2014-11-30 22:09:56 +0100

    biggy added

---
+++ kokos/kokos.Communication/Store/SymbolDb.cs
@@ -0,0 +1,15 @@
+﻿using Biggy.Core;
+using Biggy.Data.Json;
+
+namespace kokos.Communication.Store
+{
+    public class SymbolDb
+    {
+        public BiggyList<SymbolStore> Symbols { get; protected set; }
+
+        public SymbolDb()
+        {
+            Symbols = new BiggyList<SymbolStore>(new JsonStore<SymbolStore>("symboldata"));
+        }
+    }
+}
---
+++ kokos/kokos.Communication/Store/SymbolStore.cs
@@ -0,0 +1,13 @@
+﻿using kokos.Abstractions;
+using System.Collections.Generic;
+
+namespace kokos.Communication.Store
+{
+    public class SymbolStore
+    {
+        public Symbol Symbol { get; set; }
+        public List<TickData> DailyTickData { get; set; }
+        public List<TickData> IntradayTickData { get; set; }
+ 
+    }
+}
